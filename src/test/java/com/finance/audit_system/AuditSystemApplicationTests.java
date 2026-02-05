ad884fc7307ca88edbb7b13db2a9563c75b99bd3
haad736 <haad736@MacBook-Air.local>
2026-02-05 06:05:18 -0600

    Initialize Project

---
+++ src/test/java/com/finance/audit_system/AuditSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.audit_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
