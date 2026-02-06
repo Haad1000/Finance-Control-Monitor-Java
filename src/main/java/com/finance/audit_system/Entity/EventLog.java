9ce90d986d40633efce5ff97089620179577e710
haad736 <haad736@gmail.com>
2026-02-06 01:14:22 -0600

    Create Eventlog entity + Minor fix in Transaction entity

---
+++ src/main/java/com/finance/audit_system/Entity/EventLog.java
@@ -0,0 +1,25 @@
+package com.finance.audit_system.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "event_logs")
+@Data
+public class EventLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String action; // Login or creating a transaction etc.
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(columnDefinition = "TEXT")
+    private String metadata; // The details, the paragraph of information
+}
--- src/main/java/com/finance/audit_system/Entity/Transaction.java
+++ src/main/java/com/finance/audit_system/Entity/Transaction.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 public class Transaction {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
