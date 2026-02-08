c8bc4a2accb3df0a249114eba454314d69df8b8c
haad736 <haad736@gmail.com>
2026-02-08 04:25:41 -0600

    Fix small bug

--- src/main/java/com/finance/audit_system/dao/EventLogRepository.java
+++ src/main/java/com/finance/audit_system/dao/EventLogRepository.java
@@ -1,12 +1,7 @@
 package com.finance.audit_system.dao;
 
 import com.finance.audit_system.Entity.EventLog;
-import com.finance.audit_system.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface EventLogRepository extends JpaRepository<EventLog, Long> {
-
-    Optional<User> findUserByUserName(String username);
 }
