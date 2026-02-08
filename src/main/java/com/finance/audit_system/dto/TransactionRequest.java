1fa08df423d4b4cb2fe15b56a7921af0df1006e4
haad736 <haad736@gmail.com>
2026-02-08 04:16:13 -0600

    Create Data Transfer Objects

---
+++ src/main/java/com/finance/audit_system/dto/TransactionRequest.java
@@ -0,0 +1,21 @@
+package com.finance.audit_system.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class TransactionRequest {
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.01", message = "Amount must be greater than 0")
+    private BigDecimal amount;
+
+    @NotBlank(message = "Type is required")
+    private String type;
+
+    @NotBlank(message = "User ID is required")
+    private String username;
+}
