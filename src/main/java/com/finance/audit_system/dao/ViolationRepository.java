4464ebdc12f08be35ca0825c13d5ae94b7025bfe
haad736 <haad736@gmail.com>
2026-02-11 14:00:27 -0600

    Audit Engine Complete with Scheduler

--- src/main/java/com/finance/audit_system/dao/ViolationRepository.java
+++ src/main/java/com/finance/audit_system/dao/ViolationRepository.java
@@ -1,7 +1,10 @@
 package com.finance.audit_system.dao;
 
+import com.finance.audit_system.Entity.EventLog;
 import com.finance.audit_system.Entity.Violation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ViolationRepository extends JpaRepository<Violation, Long> {
+
+    boolean existsByRelatedEvent(EventLog eventLog);
 }
--- src/main/java/com/finance/audit_system/service/AuditEngine.java
+++ src/main/java/com/finance/audit_system/service/AuditEngine.java
@@ -1,4 +1,46 @@
 package com.finance.audit_system.service;
 
+import com.finance.audit_system.Entity.EventLog;
+import com.finance.audit_system.Entity.Violation;
+import com.finance.audit_system.audit.AuditRule;
+import com.finance.audit_system.dao.EventLogRepository;
+import com.finance.audit_system.dao.ViolationRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AuditEngine {
+
+    private final EventLogRepository eventLogRepository;
+    private final ViolationRepository violationRepository;
+    private final List<AuditRule> auditRules;
+
+    public AuditEngine(EventLogRepository eventLogRepository, ViolationRepository violationRepository, List<AuditRule> auditRules) {
+        this.eventLogRepository = eventLogRepository;
+        this.violationRepository = violationRepository;
+        this.auditRules = auditRules;
+    }
+
+    @Scheduled(fixedDelay = 10000)
+    public void runAudit() {
+        System.out.println("Audit Engine: Scanning for rule violations");
+
+        List<EventLog> eventLogs = eventLogRepository.findAll();
+
+        for (AuditRule rules: auditRules) {
+
+            List<Violation> violations = rules.evaluate(eventLogs);
+
+            for (Violation v : violations) {
+                boolean alreadyExists = violationRepository.existsByRelatedEvent(v.getRelatedEvent());
+
+                if (!alreadyExists) {
+                    System.out.println("NEW VIOLATION FOUND: " + rules.getRuleId());
+                    violationRepository.save(v);
+                }
+            }
+        }
+    }
 }
